93fee550b28e4bcd4d0955c26c570ad8d7a23200
Cato <catodekruif@gmail.com>
2022-07-06 17:29:23 +0200

    changes

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             int a = 1;
-            int b = 2;
+            int b = 3;
             int c = a + b;
             System.Console.WriteLine(c);
         }
