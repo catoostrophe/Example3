b4fbaf915ec33e39db13b640441f9f10ae448532
Cato <catodekruif@gmail.com>
2022-07-06 17:19:06 +0200

    init

---
+++ Example3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Example3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int d = 4;
+            int e = a + b;
+            System.Console.WriteLine(e);
+        }
+    }
+}
